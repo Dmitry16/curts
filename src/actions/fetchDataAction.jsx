85c5f8f9187c25ee0afe8905de558b0bc32d8f29
Dmitry16 <europartner@inbox.ru>
2018-10-22 20:43:28 +0300

    regit

---
+++ src/actions/fetchDataAction.jsx
@@ -0,0 +1,19 @@
+
+export function fetchData() {
+
+  // return function(dispatch) {
+  //   return axios.get('data/response.json')
+  //     .then((response) => {
+  //       setTimeout(function(){
+  //
+  //         dispatch({type: 'FETCH_DATA_FULFILLED',
+  //         payload: tripsFilter(response.data, ...searchParams)});
+  //
+  //       }, 2000);
+  //     })
+  //     .catch((err) => {
+  //       dispatch({type: 'FETCH_DATA_REJECTED',
+  //                 payload: err});
+  //     })
+  // }
+}
